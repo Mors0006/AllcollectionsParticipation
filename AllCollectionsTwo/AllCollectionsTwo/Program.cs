c1e09ef8a704ea8fe0b8e45525df3a6a2f087c56
Kyle Morse <Kylejmorse@ou.edu>
2021-03-25 16:14:13 -0500

    Collections Two Done

---
+++ AllCollectionsTwo/AllCollectionsTwo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AllCollectionsTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer;
+
+            List<double> gradeScores = new List<double>();
+            
+
+            do
+            {
+                Console.WriteLine("Enter your grade: >>");
+                answer = Console.ReadLine();
+
+                double score = Convert.ToDouble(answer);
+                gradeScores.Add(score);
+
+                Console.WriteLine("Do you have another exam score to enter, yes or no? >>");
+                answer = Console.ReadLine();
+
+            } while (answer.ToLower()[0] == 'y');
+            double sum = 0;
+            foreach (double score in gradeScores)
+            {
+                sum += score;
+            }
+            double average = sum / gradeScores.Count;
+            Console.WriteLine($"Your average grade is {average}");
+
+
+        }
+    }
+}
